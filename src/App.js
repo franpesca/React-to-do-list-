a8d909553347de51a119db2ed157e3cafa6508fc
FrancescaCitter <fra.citter@gmail.com>
2018-05-28 12:19:32 +0200

    disabled check list and pop up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment  } from 'react';
+import Popup from './popup.js'
+import './App.css'
+
+
 
 class App extends Component {
   constructor(props){
     super(props)
 
-    this.state = { 
+    this.state = {
       userInput: '',
       list: [],
+      disable: false
     }
   }
+
+  handleArrLength() {
+    this.setState( 
+      {
+        disabled: !this.state.disabled
+      } 
+    )
+  } 
 
   changeUserInput(input){
     this.setState(
@@ -20,39 +32,101 @@ class App extends Component {
   }
 
   addToDoList(input){
-    let listArray = this.state.list
-    listArray.push(input);
+    const listArray = [ ...this.state.list, input]
 
     this.setState({
       list: listArray,
       userInput: '',
+      counter: this.state.counter + 1
     }
     )
   }
 
+  //click = index nel render, posiione elem nell array 
+  delateInput(click){
+
+    const newList = [ ...this.state.list]
+
+    newList.splice(click,1)
+
+    this.setState(
+      {
+        list: newList
+      }
+    )
+    }
+
+
+    maxArr(click){
+
+      const limitedList = [...this.state.list]
+
+      limitedList.splice(0,9)
+
+      this.setState({
+        list: limitedList
+      })
+    }
+
 
   render() {
-    return (
-// aggiungi titolo il H1, 
+
+
+    return ( 
+      <div className='popup'>
+      {(this.state.list.length >= 10 )? <Popup/> : null}
       <div className='to-do-list'>
 
+        <h1> My To-Do List </h1>
         <input
           onChange={ (e) => this.changeUserInput(e.target.value)}
           value={this.state.userInput}
           type='text'
           placeholder='write here your to-do list'
-          onKeyUp={ (event) => event.keyCode === 13 ? this.addToDoList(this.state.userInput): null}
+          onKeyUp= {(event) => event.keyCode === 13 ? this.addToDoList(this.state.userInput): null}
+
+          disabled= {this.state.list.length === 10 ? true : false}
+          //disabled={(this.state.list.length = 10) ? true : false}
+          //disabled= {!this.props.isEditMode}
+          
+          // onClick= { (event) => this.state.list.length <= 10? this.handleArrLength.bind(this): null}>
        />
     
         <button 
-          className='button'
+          className='myButton'
           onClick={ () => this.addToDoList(this.state.userInput)}
-          > What's next? </button>
+          > What's next? 
+        </button>
+
         <div className='list'>
-          <ul>
-            {this.state.list.map( (val)=> <li> {val} </li> )}
-          </ul>
+          <ol>
+            {this.state.list.map( (val, index) => 
+                
+              // {/* fragment non viene renderizzato  */}
+              
+              <li key={"listE" + index}> 
+              
+              <span> {index+1}</span>
+              <span> {val} </span>
+              <button 
+              data-id={index}    //lo uso per posizioni giuste, e' di default..poi lo richiamo con dataset.id
+              className= 'button-yo'
+              // onClick= {(e) => {this.setState({list: this.state.list.splice(e.target.attributes.attr.value,1)})}}
+              onClick = { (e) => this.delateInput(e.target.dataset.id)}
+              > 
+              <span>&#9996;</span>
+              
+              </button> 
+ 
+              </li>
+            
+            )}
+              
+          </ol>
         </div>
+      </div>
+
+
       </div>
       
 
---
+++ src/popup.js
@@ -0,0 +1,14 @@
+import React, { Component  } from 'react';
+import './popup.css';
+
+class Popup extends Component {
+    
+    render() {
+        return (
+            <div>
+                <h1> STOP PROCRASTINATING, A REAL BOMBER ALWAYS DOES WHAT HE HAS TO!</h1>
+            </div>
+         )
+        }
+    }
+    export default Popup;
